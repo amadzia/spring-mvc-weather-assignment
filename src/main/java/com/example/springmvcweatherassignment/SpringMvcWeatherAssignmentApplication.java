eadac21c19bb6f485af5462231e9673adca73e9c
amadzia <agnieszkamadzia@onet.pl>
2018-10-17 08:42:36 +0200

    first commit

---
+++ src/main/java/com/example/springmvcweatherassignment/SpringMvcWeatherAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.example.springmvcweatherassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcWeatherAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcWeatherAssignmentApplication.class, args);
+    }
+}
---
+++ src/test/java/com/example/springmvcweatherassignment/SpringMvcWeatherAssignmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springmvcweatherassignment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringMvcWeatherAssignmentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
