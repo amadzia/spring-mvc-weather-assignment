9c30b4e440a2f0fbd955f125a85ee509c841c2c2
amadzia <agnieszkamadzia@onet.pl>
2018-10-18 11:56:47 +0200

    Swagger and controller layer added

---
+++ src/main/java/com/example/springmvcweatherassignment/controllers/v1/WeatherController.java
@@ -0,0 +1,46 @@
+package com.example.springmvcweatherassignment.controllers.v1;
+
+import com.example.api.v1.domain.WeatherForecast;
+import com.example.springmvcweatherassignment.services.ApiService;
+import org.springframework.http.MediaType;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+@RestController
+@RequestMapping(WeatherController.BASE_URI)
+public class WeatherController {
+
+    public static final String BASE_URI = "/api/v1";
+
+
+    private ApiService apiService;
+
+    public WeatherController(ApiService apiService) {
+        this.apiService = apiService;
+    }
+
+    @GetMapping(value = "/weatherforecast", produces = MediaType.APPLICATION_JSON_VALUE)
+    public WeatherForecast getWeatherForecastForPreDefinedCityAndCountryCode() {
+
+        return apiService.getWeatherForecast("London", "UK");
+    }
+
+
+    @PostMapping(value = "/weatherforecast")
+    public WeatherForecast formPost(@RequestParam String city, @RequestParam String countryCode, Model model, HttpServletRequest request) {
+
+        WeatherForecast weatherForecast = new WeatherForecast();
+
+        Map<String, String[]> map = request.getParameterMap();
+        weatherForecast.setCityName(String.valueOf(map.get("city")));
+        weatherForecast.setCountryCode(String.valueOf(map.get("countryCode")));
+
+        model.addAttribute("weatherforecast", apiService.getWeatherForecast(city, countryCode));
+
+        return apiService.getWeatherForecast(city, countryCode);
+    }
+
+}
