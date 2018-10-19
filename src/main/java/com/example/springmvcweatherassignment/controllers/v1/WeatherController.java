c7eddd9de065f8e075aa5937b74133737e852348
amadzia <agnieszkamadzia@onet.pl>
2018-10-19 11:39:24 +0200

    Cleaning up the code from unnecessary elements. Setting up the TTL parameter to the requested value

--- src/main/java/com/example/springmvcweatherassignment/controllers/v1/WeatherController.java
+++ src/main/java/com/example/springmvcweatherassignment/controllers/v1/WeatherController.java
@@ -4,7 +4,10 @@ import com.example.api.v1.domain.WeatherForecast;
 import com.example.springmvcweatherassignment.services.ApiService;
 import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(WeatherController.BASE_URI)
@@ -18,13 +21,6 @@ public class WeatherController {
     public WeatherController(ApiService apiService) {
         this.apiService = apiService;
     }
-
-    @GetMapping(value = "/weatherforecast", produces = MediaType.APPLICATION_JSON_VALUE)
-    public WeatherForecast getWeatherForecastForPreDefinedCityAndCountryCode() {
-
-        return apiService.getWeatherForecast("London", "UK");
-    }
-
 
     @PostMapping(value = "/weatherforecast", produces = MediaType.APPLICATION_JSON_VALUE)
     public WeatherForecast formPost(@RequestParam String city, @RequestParam String countryCode, Model model) {
